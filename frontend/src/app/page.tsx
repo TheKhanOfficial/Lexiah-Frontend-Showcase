5827b08e879acf16737759435885b0e43698b717
TheKhanOfficial <victordavidkhan@proton.me>
2025-07-24 21:38:52 -0400

    please

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Lexiah</h1>
+      <p>
+        Go to your user page:{" "}
+        <Link href="/53917586-97ad-49b6-9bd6-51c441316425">Click here</Link>
+      </p>
+    </main>
+  );
+}
